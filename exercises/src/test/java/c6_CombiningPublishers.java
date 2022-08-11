698d45a3ecb83e5625644cdef2591920db101260
Julien Lin <lin.julien@hotmail.com>
2022-08-10 23:40:56 -0400

    c6

--- exercises/src/test/java/c6_CombiningPublishers.java
+++ exercises/src/test/java/c6_CombiningPublishers.java
@@ -11,7 +11,8 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 /**
- * In this important chapter we are going to cover different ways of combining publishers.
+ * In this important chapter we are going to cover different ways of combining
+ * publishers.
  *
  * Read first:
  *
@@ -37,17 +38,18 @@ public class c6_CombiningPublishers extends CombiningPublishersBase {
      */
     @Test
     public void behold_flatmap() {
-        Hooks.enableContextLossTracking(); //used for testing - detects if you are cheating!
+        Hooks.enableContextLossTracking(); // used for testing - detects if you are cheating!
 
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         Mono<String> currentUserEmail = null;
         Mono<String> currentUserMono = getCurrentUser();
-        getUserEmail(null);
 
-        //don't change below this line
+        currentUserEmail = currentUserMono.flatMap(this::getUserEmail);
+
+        // don't change below this line
         StepVerifier.create(currentUserEmail)
-                    .expectNext("user123@gmail.com")
-                    .verifyComplete();
+                .expectNext("user123@gmail.com")
+                .verifyComplete();
     }
 
     /**
@@ -59,53 +61,51 @@ public class c6_CombiningPublishers extends CombiningPublishersBase {
      */
     @Test
     public void task_executor() {
-        //todo: feel free to change code as you need
-        Flux<Void> tasks = null;
-        taskExecutor();
+        // todo: feel free to change code as you need
+        Flux<Void> tasks = taskExecutor().flatMap(Function.identity());
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(tasks)
-                    .verifyComplete();
+                .verifyComplete();
 
         Assertions.assertEquals(taskCounter.get(), 10);
     }
 
     /**
      * `streamingService()` opens a connection to the data provider.
-     * Once connection is established you will be able to collect messages from stream.
+     * Once connection is established you will be able to collect messages from
+     * stream.
      *
      * Establish connection and get all messages from data provider stream!
      */
     @Test
     public void streaming_service() {
-        //todo: feel free to change code as you need
-        Flux<Message> messageFlux = null;
-        streamingService();
+        // todo: feel free to change code as you need
+        Flux<Message> messageFlux = streamingService().flatMapMany(Function.identity());
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(messageFlux)
-                    .expectNextCount(10)
-                    .verifyComplete();
+                .expectNextCount(10)
+                .verifyComplete();
     }
 
-
     /**
-     * Join results from services `numberService1()` and `numberService2()` end-to-end.
-     * First `numberService1` emits elements and then `numberService2`. (no interleaving)
+     * Join results from services `numberService1()` and `numberService2()`
+     * end-to-end.
+     * First `numberService1` emits elements and then `numberService2`. (no
+     * interleaving)
      *
      * Bonus: There are two ways to do this, check out both!
      */
     @Test
     public void i_am_rubber_you_are_glue() {
-        //todo: feel free to change code as you need
-        Flux<Integer> numbers = null;
-        numberService1();
-        numberService2();
+        // todo: feel free to change code as you need
+        Flux<Integer> numbers = Flux.concat(numberService1(), numberService2());
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(numbers)
-                    .expectNext(1, 2, 3, 4, 5, 6, 7)
-                    .verifyComplete();
+                .expectNext(1, 2, 3, 4, 5, 6, 7)
+                .verifyComplete();
     }
 
     /**
@@ -123,116 +123,121 @@ public class c6_CombiningPublishers extends CombiningPublishersBase {
      */
     @Test
     public void task_executor_again() {
-        //todo: feel free to change code as you need
-        Flux<Void> tasks = null;
-        taskExecutor();
+        // todo: feel free to change code as you need
+        Flux<Void> tasks = taskExecutor().concatMap(Function.identity());
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(tasks)
-                    .verifyComplete();
+                .verifyComplete();
 
         Assertions.assertEquals(taskCounter.get(), 10);
     }
 
     /**
-     * You are writing software for broker house. You can retrieve current stock prices by calling either
+     * You are writing software for broker house. You can retrieve current stock
+     * prices by calling either
      * `getStocksGrpc()` or `getStocksRest()`.
-     * Since goal is the best response time, invoke both services but use result only from the one that responds first.
+     * Since goal is the best response time, invoke both services but use result
+     * only from the one that responds first.
      */
     @Test
     public void need_for_speed() {
-        //todo: feel free to change code as you need
-        Flux<String> stonks = null;
-        getStocksGrpc();
-        getStocksRest();
+        // todo: feel free to change code as you need
+        Flux<String> stonks = Flux.merge(getStocksGrpc(), getStocksRest());
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(stonks)
-                    .expectNextCount(5)
-                    .verifyComplete();
+                .expectNextCount(5)
+                .verifyComplete();
     }
 
     /**
-     * As part of your job as software engineer for broker house, you have also introduced quick local cache to retrieve
-     * stocks from. But cache may not be formed yet or is empty. If cache is empty, switch to a live source:
+     * As part of your job as software engineer for broker house, you have also
+     * introduced quick local cache to retrieve
+     * stocks from. But cache may not be formed yet or is empty. If cache is empty,
+     * switch to a live source:
      * `getStocksRest()`.
      */
     @Test
     public void plan_b() {
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         Flux<String> stonks = null;
         getStocksLocalCache();
         getStocksRest();
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(stonks)
-                    .expectNextCount(6)
-                    .verifyComplete();
+                .expectNextCount(6)
+                .verifyComplete();
 
         Assertions.assertTrue(localCacheCalled.get());
     }
 
     /**
-     * You are checking mail in your mailboxes. Check first mailbox, and if first message contains spam immediately
+     * You are checking mail in your mailboxes. Check first mailbox, and if first
+     * message contains spam immediately
      * switch to a second mailbox. Otherwise, read all messages from first mailbox.
      */
     @Test
     public void mail_box_switcher() {
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         Flux<Message> myMail = null;
         mailBoxPrimary();
         mailBoxSecondary();
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(myMail)
-                    .expectNextMatches(m -> !m.metaData.equals("spam"))
-                    .expectNextMatches(m -> !m.metaData.equals("spam"))
-                    .verifyComplete();
+                .expectNextMatches(m -> !m.metaData.equals("spam"))
+                .expectNextMatches(m -> !m.metaData.equals("spam"))
+                .verifyComplete();
 
         Assertions.assertEquals(1, consumedSpamCounter.get());
     }
 
     /**
      * You are implementing instant search for software company.
-     * When user types in a text box results should appear in near real-time with each keystroke.
+     * When user types in a text box results should appear in near real-time with
+     * each keystroke.
      *
      * Call `autoComplete()` function for each user input
-     * but if newer input arrives, cancel previous `autoComplete()` call and call it for latest input.
+     * but if newer input arrives, cancel previous `autoComplete()` call and call it
+     * for latest input.
      */
     @Test
     public void instant_search() {
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         autoComplete(null);
         Flux<String> suggestions = userSearchInput()
-                //todo: use one operator only
-                ;
+        // todo: use one operator only
+        ;
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(suggestions)
-                    .expectNext("reactor project", "reactive project")
-                    .verifyComplete();
+                .expectNext("reactor project", "reactive project")
+                .verifyComplete();
     }
-
 
     /**
      * Code should work, but it should also be easy to read and understand.
-     * Orchestrate file writing operations in a self-explanatory way using operators like `when`,`and`,`then`...
-     * If all operations have been executed successfully return boolean value `true`.
+     * Orchestrate file writing operations in a self-explanatory way using operators
+     * like `when`,`and`,`then`...
+     * If all operations have been executed successfully return boolean value
+     * `true`.
      */
     @Test
     public void prettify() {
-        //todo: feel free to change code as you need
-        //todo: use when,and,then...
+        // todo: feel free to change code as you need
+        // todo: use when,and,then...
         Mono<Boolean> successful = null;
 
         openFile();
         writeToFile("0x3522285912341");
         closeFile();
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(successful)
-                    .expectNext(true)
-                    .verifyComplete();
+                .expectNext(true)
+                .verifyComplete();
 
         Assertions.assertTrue(fileOpened.get());
         Assertions.assertTrue(writtenToFile.get());
@@ -244,134 +249,138 @@ public class c6_CombiningPublishers extends CombiningPublishersBase {
      */
     @Test
     public void one_to_n() {
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         Flux<String> fileLines = null;
         openFile();
         readFile();
 
         StepVerifier.create(fileLines)
-                    .expectNext("0x1", "0x2", "0x3")
-                    .verifyComplete();
+                .expectNext("0x1", "0x2", "0x3")
+                .verifyComplete();
     }
 
     /**
-     * Execute all tasks sequentially and after each task have been executed, commit task changes. Don't lose id's of
+     * Execute all tasks sequentially and after each task have been executed, commit
+     * task changes. Don't lose id's of
      * committed tasks, they are needed to further processing!
      */
     @Test
     public void acid_durability() {
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         Flux<String> committedTasksIds = null;
         tasksToExecute();
         commitTask(null);
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(committedTasksIds)
-                    .expectNext("task#1", "task#2", "task#3")
-                    .verifyComplete();
+                .expectNext("task#1", "task#2", "task#3")
+                .verifyComplete();
 
         Assertions.assertEquals(3, committedTasksCounter.get());
     }
 
-
     /**
-     * News have come that Microsoft is buying Blizzard and there will be a major merger.
-     * Merge two companies, so they may still produce titles in individual pace but as a single company.
+     * News have come that Microsoft is buying Blizzard and there will be a major
+     * merger.
+     * Merge two companies, so they may still produce titles in individual pace but
+     * as a single company.
      */
     @Test
     public void major_merger() {
-        //todo: feel free to change code as you need
-        Flux<String> microsoftBlizzardCorp =
-                microsoftTitles();
+        // todo: feel free to change code as you need
+        Flux<String> microsoftBlizzardCorp = microsoftTitles();
         blizzardTitles();
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(microsoftBlizzardCorp)
-                    .expectNext("windows12",
-                                "wow2",
-                                "bing2",
-                                "overwatch3",
-                                "office366",
-                                "warcraft4")
-                    .verifyComplete();
+                .expectNext("windows12",
+                        "wow2",
+                        "bing2",
+                        "overwatch3",
+                        "office366",
+                        "warcraft4")
+                .verifyComplete();
     }
 
-
     /**
-     * Your job is to produce cars. To produce car you need chassis and engine that are produced by a different
+     * Your job is to produce cars. To produce car you need chassis and engine that
+     * are produced by a different
      * manufacturer. You need both parts before you can produce a car.
-     * Also, engine factory is located further away and engines are more complicated to produce, therefore it will be
+     * Also, engine factory is located further away and engines are more complicated
+     * to produce, therefore it will be
      * slower part producer.
      * After both parts arrive connect them to a car.
      */
     @Test
     public void car_factory() {
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         Flux<Car> producedCars = null;
         carChassisProducer();
         carEngineProducer();
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(producedCars)
-                    .recordWith(ConcurrentLinkedDeque::new)
-                    .expectNextCount(3)
-                    .expectRecordedMatches(cars -> cars.stream()
-                                                       .allMatch(car -> Objects.equals(car.chassis.getSeqNum(),
-                                                                                       car.engine.getSeqNum())))
-                    .verifyComplete();
+                .recordWith(ConcurrentLinkedDeque::new)
+                .expectNextCount(3)
+                .expectRecordedMatches(cars -> cars.stream()
+                        .allMatch(car -> Objects.equals(car.chassis.getSeqNum(),
+                                car.engine.getSeqNum())))
+                .verifyComplete();
     }
 
     /**
-     * When `chooseSource()` method is used, based on current value of sourceRef, decide which source should be used.
+     * When `chooseSource()` method is used, based on current value of sourceRef,
+     * decide which source should be used.
      */
 
-    //only read from sourceRef
+    // only read from sourceRef
     AtomicReference<String> sourceRef = new AtomicReference<>("X");
 
-    //todo: implement this method based on instructions
+    // todo: implement this method based on instructions
     public Mono<String> chooseSource() {
-        sourceA(); //<- choose if sourceRef == "A"
-        sourceB(); //<- choose if sourceRef == "B"
-        return Mono.empty(); //otherwise, return empty
+        sourceA(); // <- choose if sourceRef == "A"
+        sourceB(); // <- choose if sourceRef == "B"
+        return Mono.empty(); // otherwise, return empty
     }
 
     @Test
     public void deterministic() {
-        //don't change below this line
+        // don't change below this line
         Mono<String> source = chooseSource();
 
         sourceRef.set("A");
         StepVerifier.create(source)
-                    .expectNext("A")
-                    .verifyComplete();
+                .expectNext("A")
+                .verifyComplete();
 
         sourceRef.set("B");
         StepVerifier.create(source)
-                    .expectNext("B")
-                    .verifyComplete();
+                .expectNext("B")
+                .verifyComplete();
     }
 
     /**
      * Sometimes you need to clean up after your self.
-     * Open a connection to a streaming service and after all elements have been consumed,
+     * Open a connection to a streaming service and after all elements have been
+     * consumed,
      * close connection (invoke closeConnection()), without blocking.
      *
      * This may look easy...
      */
     @Test
     public void cleanup() {
-        BlockHound.install(); //don't change this line, blocking = cheating!
+        BlockHound.install(); // don't change this line, blocking = cheating!
 
-        //todo: feel free to change code as you need
+        // todo: feel free to change code as you need
         Flux<String> stream = StreamingConnection.startStreaming()
-                                                 .flatMapMany(Function.identity());
+                .flatMapMany(Function.identity());
         StreamingConnection.closeConnection();
 
-        //don't change below this line
+        // don't change below this line
         StepVerifier.create(stream)
-                    .then(()-> Assertions.assertTrue(StreamingConnection.isOpen.get()))
-                    .expectNextCount(20)
-                    .verifyComplete();
+                .then(() -> Assertions.assertTrue(StreamingConnection.isOpen.get()))
+                .expectNextCount(20)
+                .verifyComplete();
         Assertions.assertTrue(StreamingConnection.cleanedUp.get());
     }
 }
